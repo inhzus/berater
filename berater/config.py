eb338ea0e172ea2e9fcc068ce56ad63eab7d8a2d
Zhi Sun <zhi.suun@gmail.com>
2019-04-14 18:06:55 +0800

    [Feat] Create wechat menu

--- berater/config.py
+++ berater/config.py
@@ -2,6 +2,7 @@
 # created by inhzus
 
 
+from berater.utils.wechat_sdk import Url
 # noinspection PyUnresolvedReferences
 from .secret import (API_KEY, API_SECRET)
 
@@ -9,6 +10,7 @@ from .secret import (API_KEY, API_SECRET)
 class BaseConfig(object):
     PROJECT = 'Berater'
     WECHAT_TOKEN = 'bkzs'
+
     @staticmethod
     def init_app(app):
         pass
@@ -16,6 +18,7 @@ class BaseConfig(object):
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
+    SERVER_URL = 'http://weixinbak.njunova.com'
     # noinspection SpellCheckingInspection
     CRYPTO_KEY = b'ha0jkDDbnn9PT0UKCz1eCZjhVvCVwYWpaG5x2T_P1xo='
 
@@ -27,4 +30,16 @@ class ProductionConfig(BaseConfig):
 config = {
     'dev': DevelopmentConfig,
     'pro': ProductionConfig
+}
+
+MENU = {
+    'button': {
+        {
+            'type': 'view',
+            'name': '测试按钮',
+            'url': Url.oauth2_auth_token.format(
+                appid=API_KEY,
+                redirect_url='{}/chat/test'.format(DevelopmentConfig.SERVER_URL))
+        }
+    }
 }
---
+++ berater/menu.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# created by inhzus
+
+from berater.utils.wechat_sdk import AccessToken, Url, Communicate
+from berater.config import API_KEY, API_SECRET, MENU
+import json
+
+
+def create_menu():
+    token = AccessToken.get(API_KEY, API_SECRET)
+    if token:
+        url = Url.create_menu.format(access_token=token)
+        return 'errcode' in Communicate.post(url, data=json.dumps(MENU, ensure_ascii=False).encode('utf-8'))
+    return False
+
+
+if __name__ == '__main__':
+    print('Menu create' if create_menu() else 'Menu create failed')
