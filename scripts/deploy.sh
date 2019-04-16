94012d97a30ee479d880b17b14e5aa5c38c9bee6
Zhi Sun <zhi.suun@gmail.com>
2019-04-16 12:33:14 +0800

    [Build] Force pull from origin master

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 source env/Scripts/activate
 pip3 freeze > req.txt
-scp berater/secret.py req.txt suun@weixinbak.njunova.com:~/berater/berater
+scp berater/secret.py req.txt suun@weixinbak.njunova.com:~/
 rm req.txt
 ssh suun@weixinbak.njunova.com "
 cd ~/berater;
 source env/bin/activate;
-git pull;
-pip3 install -r berater/req.txt;
-rm berater/req.txt;
+git fetch;
+git reset origin/master --hard;
+git clean -df;
+mv ~/secret.py berater/secret.py;
+pip3 install -r ~/req.txt;
+rm ~/req.txt;
 "
