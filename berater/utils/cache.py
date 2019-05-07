3dd212627a49e8503d62b98c1f5889174925eac2
Zhi Sun <inhzus@gmail.com>
2019-05-07 16:13:09 +0800

    [Feat] MemoryCache refresh expire time

--- berater/utils/cache.py
+++ berater/utils/cache.py
@@ -6,29 +6,33 @@ import time
 import weakref
 
 
+class Entity(object):
+    def __init__(self, ttl: int, data: dict):
+        self.expire = (time.time() + ttl) if ttl else 0
+        self.data = data
+
+    def is_expired(self):
+        return self.expire and time.time() > self.expire
+
+    def refresh(self, ttl):
+        self.expire = (time.time() + ttl) if ttl else 0
+
+
 class MemoryCache(object):
-    class Entity(object):
-        def __init__(self, ttl: int, data: dict):
-            self.expire = (time.time() + ttl) if ttl else 0
-            self.data = data
-
-        def is_expired(self):
-            return self.expire and time.time() > self.expire
-
     def __init__(self, ttl: int, max_len: int = 0):
         self.ttl = ttl
         self.weak = weakref.WeakValueDictionary()
         self.strong = collections.deque(maxlen=max_len) if max_len else collections.deque()
 
     def get(self, key, default=None) -> dict:
-        entity: MemoryCache.Entity = self.weak.get(key, None)
+        entity: Entity = self.weak.get(key, None)
         if entity is not None:
             if not entity.is_expired():
                 return entity.data
         return default
 
     def set(self, key, **kwargs):
-        self.weak[key] = ref = MemoryCache.Entity(self.ttl, kwargs)
+        self.weak[key] = ref = Entity(self.ttl, kwargs)
         self.strong.append(ref)
         while self.strong[0].is_expired():
             self.strong.popleft()
@@ -40,3 +44,8 @@ class MemoryCache(object):
                 data[k] = v
             return True
         return False
+
+    def refresh(self, key):
+        entity: Entity = self.weak.get(key, None)
+        if entity is not None:
+            entity.refresh(self.ttl)
