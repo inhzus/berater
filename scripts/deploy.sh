a8b625c6fba7441074ee8b0e5c097de3132118cb
Zhi Sun <inhzus@gmail.com>
2019-05-22 17:21:27 +0800

    [Build] Deploy scripts with docker

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 source env/bin/activate
-scp berater/secret.py suun@weixinbak.njunova.com:~/
+scp .env suun@weixinbak.njunova.com:~/
 ssh suun@weixinbak.njunova.com "
 cd ~/berater;
-source env/bin/activate;
 git fetch;
 git reset origin/master --hard;
 git clean -df;
-mv ~/secret.py berater/secret.py;
-pip3 install -r requirements.txt;
+mv ~/.env ./;
 "
--- scripts/initial.sh
+++ scripts/initial.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
-source env/bin/activate
-scp berater/secret.py suun@weixinbak.njunova.com:~/
+scp .env suun@weixinbak.njunova.com:~/
 ssh suun@weixinbak.njunova.com "
 rm -rf berater;
 git clone https://github.com/inhzus/berater;
 cd berater;
-mv ~/secret.py berater/;
-sudo apt-get install virtualenv;
-virtualenv env -p python3;
-source env/bin/activate;
-pip3 install -r requirements.txt;
+mv ~/.env ./;
 "
