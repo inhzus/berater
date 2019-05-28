feb523abac25027212282142d9ac8b0ee7fe418f
Zhi Sun <inhzus@gmail.com>
2019-05-28 14:37:54 +0800

    [Fix] Source student name column to dict

--- berater/misc/models.py
+++ berater/misc/models.py
@@ -56,6 +56,6 @@ class SourceStudentTable(engine.Model):
     admission_id = engine.Column(engine.String(20), index=True)
 
     def to_dict(self):
-        return dict(stuid=self.stuid, origin=self.origin,
+        return dict(stuid=self.stuid, origin=self.origin, name=self.name,
                     gender=self.gender, department=self.department,
                     id_card=self.id_card, admission_id=self.admission_id)
