adbae20bb52e714a81f5cea30df7835cdd1c5cff
sunzhi <sunzhi.im@bytedance.com>
2019-07-14 19:52:33 +0800

    [Feat] Catch internal erros globally & print stack in log

--- berater/api/views.py
+++ berater/api/views.py
@@ -56,7 +56,7 @@ def check_token():
             StudentTable.openid == current_identity.openid).first()
         admission_id = stuid = ''
         if student:
-            source_student: SourceStudentTable = session.query(SourceStudentTable.stuid).filter(
+            source_student: SourceStudentTable = session.query(SourceStudentTable.stuid, SourceStudentTable.admission_id).filter(
                 SourceStudentTable.id_card == student.id_card).first()
             if source_student:
                 stuid = source_student.stuid
--- berater/exception/views.py
+++ berater/exception/views.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 # created by inhzus
 
-from flask import Blueprint, jsonify
+from traceback import extract_tb, StackSummary
+
+from flask import Blueprint, jsonify, current_app
 from werkzeug.exceptions import *
 
 error = Blueprint('errors', __name__)
@@ -9,4 +11,12 @@ error = Blueprint('errors', __name__)
 
 @error.app_errorhandler(HTTPException)
 def handle_http_exceptions(err: HTTPException):
+    current_app.logger.warning('code: {}, http exception: {}'.format(err.code, err.description))
     return jsonify(code=err.code, error=err.description, data=None)
+
+
+@error.app_errorhandler(Exception)
+def handle_exceptions(err: Exception):
+    msg = '\n'.join(['', ''.join(StackSummary.from_list(extract_tb(err.__traceback__)).format()), repr(err)])
+    current_app.logger.error(msg)
+    return jsonify(code=500, error=repr(err), data=None)
